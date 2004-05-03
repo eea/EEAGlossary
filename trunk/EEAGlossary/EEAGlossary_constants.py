0cdb355e7ba19ca4194d43ab1735a8857f269b45
finrocvs <finrocvs@24821446-e7ea-0310-81ac-b189f4507b0b>
2004-05-03 12:19:22 +0000

    development

---
+++ trunk/EEAGlossary/EEAGlossary_constants.py
@@ -0,0 +1,28 @@
+# The contents of this file are subject to the Mozilla Public
+# License Version 1.1 (the "License"); you may not use this file
+# except in compliance with the License. You may obtain a copy of
+# the License at http://www.mozilla.org/MPL/
+#
+# Software distributed under the License is distributed on an "AS
+# IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+# implied. See the License for the specific language governing
+# rights and limitations under the License.
+#
+# The Original Code is EEAGlossary version 1.0.
+#
+# The Initial Developer of the Original Code is European Environment
+# Agency (EEA).  Portions created by Finsiel Romania are
+# Copyright (C) European Environment Agency.  All
+# Rights Reserved.
+#
+# Contributor(s):
+# Alex Ghica, Finsiel Romania
+#$Id: EEAGlossary_constants.py,v 1.1 2004/05/03 12:19:22 finrocvs Exp $
+
+#META TYPES
+EEA_GLOSSARY_FOLDER_METATYPE = "EEA Glossary Folder"
+EEA_GLOSSARY_ELEMENT_METATYPE = "EEA Glossary Element"
+EEA_GLOSSARY_CENTRE_METATYPE = "EEA Glossary Centre"
+EEA_GLOSSARY_SYNONYM_METATYPE = "EEA Glossary Synonym"
+
+
