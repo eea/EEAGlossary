0196fe07cee4c3a014379b0ba0c0620d1e4ffbe7
finrocvs <finrocvs@24821446-e7ea-0310-81ac-b189f4507b0b>
2004-05-28 10:15:13 +0000

    added the export and import interface

--- trunk/EEAGlossary/EEAGlossary_export.py
+++ trunk/EEAGlossary/EEAGlossary_export.py
@@ -20,10 +20,74 @@
 # Cornel Nitu, Finsiel Romania
 #
 #
-#$Id: EEAGlossary_export.py,v 1.1 2004/05/28 08:55:00 finrocvs Exp $
+#$Id: EEAGlossary_export.py,v 1.2 2004/05/28 10:15:13 finrocvs Exp $
+
+from DateTime import DateTime
 
 class glossary_export:
     """ """
     
     def __init__(self):
         """ constructor """
+
+    def xliff_header(self, folders, language):
+        """ return the xliff header """
+        
+        results = []
+        r_append = results.append   #alias for append function. For optimization purposes
+        # Generate the XLIFF file header
+        self.REQUEST.RESPONSE.setHeader('Content-Type', 'text/xml; charset=UTF-8')
+        self.REQUEST.RESPONSE.setHeader('Content-Disposition', 'attachment; filename="%s_%s_%s.xml"' % (self.id, folders, language))
+        r_append('<?xml version="1.0" encoding="UTF-8"?>')
+        r_append('<!DOCTYPE xliff SYSTEM "http://www.oasis-open.org/committees/xliff/documents/xliff.dtd">')
+        r_append(u'<!-- XLIFF Format Copyright \xa9 OASIS Open 2001-2003 -->')
+        r_append('<xliff version="1.0">')
+        r_append('<file')
+        r_append(' original="%s"' % self.absolute_url(1))
+        r_append(' product-name="EEAGlossary"')
+        r_append(' product-version="1.1.x"')
+        r_append(' datatype="plaintext"')
+        r_append(' source-language="English"')
+        r_append(' target-language="%s"' % language)
+        r_append(' date="%s"' % DateTime().HTML4())
+        r_append('>')
+        r_append('<body>')
+        return results
+
+    def xliff_footer(self):
+        results = []
+        r_append = results.append   #alias for append function. For optimization purposes
+        r_append('</body>')
+        r_append('</file>')
+        r_append('</xliff>')
+        return results
+
+    def xliff_export(self, folders=[], language='', published=0, all_in_one=0, REQUEST=None):
+        """ Exports the content of the EEAGlossary to an XLIFF file """
+        results = []
+        terms = []
+        r_append = results.append   #alias for append function. For optimization purposes
+        for folder in self.folder_list_sorted():
+            if folder.id in self.utConvertToList(folders):
+                if published:
+                    terms.extend(self.get_published('/%s' % folder.absolute_url(1)))
+                else:
+                    terms.extend(self.get_all_objects('/%s' % folder.absolute_url(1)))
+        if all_in_one:
+            results.extend(self.xliff_header(folders, language))
+            for term in terms:
+                #if language in self.get_unicode_langs():
+                #    translation = term.get_translation_by_language(language)
+                #else:
+                #    translation = self.display_unicode_langs(term.get_translation_by_language(language), charset=self.get_language_charset(language))
+                translation = term.get_translation_by_language(language)
+                r_append('<trans-unit id="%s">' % term.id)
+                r_append(' <source>%s</source>' % term.get_translation_by_language('English'))
+                r_append(' <target>%s</target>' % translation)
+                if term.definition:
+                    r_append(' <note>%s</note>' % term.definition)
+                r_append('</trans-unit>')
+            results.extend(self.xliff_footer())
+            return '\r\n'.join(results)
+        else:
+            pass
