765c93b34446b9e64d29b852a69231349868de0d
finrocvs <finrocvs@24821446-e7ea-0310-81ac-b189f4507b0b>
2004-06-01 08:28:15 +0000

    tmx import

--- parsers/xliff_parser.py
+++ parsers/xliff_parser.py
@@ -1,24 +1,26 @@
-# -*- coding: ISO-8859-1 -*-
-# Copyright (C) 2000-2004  Juan David Ib�ez Palomar <jdavid@itaapy.com>
-#               2003  Roberto Quero, Eduardo Corrales
-#               2004  Sren Roug
+# The contents of this file are subject to the Mozilla Public
+# License Version 1.1 (the "License"); you may not use this file
+# except in compliance with the License. You may obtain a copy of
+# the License at http://www.mozilla.org/MPL/
 #
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation; either version 2
-# of the License, or (at your option) any later version.
+# Software distributed under the License is distributed on an "AS
+# IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+# implied. See the License for the specific language governing
+# rights and limitations under the License.
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# The Original Code is EEAGlossary version 1.0.0
 #
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
-
-# NOTE:
-# This parser doesn't understand the ALT-TRANS element.
+# The Initial Owner of the Original Code is European Environment
+# Agency (EEA).  Portions created by Finsiel Romania are
+# Copyright (C) European Environment Agency.  All
+# Rights Reserved.
+#
+# Contributor(s):
+# Alex Ghica, Finsiel Romania
+# Cornel Nitu, Finsiel Romania
+#
+#
+#$Id: xliff_parser.py,v 1.3 2004/06/01 08:28:15 finrocvs Exp $
 
 from xml.sax.handler import ContentHandler
 from xml.sax import *
