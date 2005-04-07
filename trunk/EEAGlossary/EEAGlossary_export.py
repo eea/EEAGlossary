0c80c0e25714d2695ae97d83d6f1f2d5b69c28bb
ghicaale <ghicaale@24821446-e7ea-0310-81ac-b189f4507b0b>
2005-04-07 14:20:27 +0000

    some prints removed

--- trunk/EEAGlossary/EEAGlossary_export.py
+++ trunk/EEAGlossary/EEAGlossary_export.py
@@ -321,7 +321,6 @@ class glossary_export:
                     #synonym relations
                     synonym_ob = l_parent_folder._getOb(l_item.id, None)
                     l_synonyms = self.getCenterId() + '/' + string.upper(l_item.synonyms[0]) + '/' + l_item.synonyms
-                    print l_synonyms
                     if string.strip(l_item.synonyms) != '':
                         synonym_ob.manageSynonymProperties(l_synonyms)
 
